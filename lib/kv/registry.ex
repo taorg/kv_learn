7579d611d872110035f7f8d166bd95bcdbd3526b
taorg <antonio.lopez@ptbsl.com>
2018-08-22 16:43:19 +0200

    Test to see the need for monitoring
    Althoght now the GenServer is monitorized handle_cast/2 implementation, you can see the registry is both linking and monitoring the buckets:
    {:ok, pid} = KV.Bucket.start_link([])
    ref = Process.monitor(pid)
    
    This is a bad idea, as we don’t want the registry to crash when a bucket crashes! We typically avoid creating new processes directly, instead, we delegate this responsibility to supervisors.

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -26,12 +26,6 @@ defmodule KV.Registry do
     GenServer.cast(server, {:create, name})
   end
 
-  ## Server Callbacks
-
-  def init(:ok) do
-    {:ok, %{}}
-  end
-
   @doc """
   Stops the registry.
   """
@@ -39,16 +33,40 @@ defmodule KV.Registry do
     GenServer.stop(server)
   end
 
-  def handle_call({:lookup, name}, _from, names) do
-    {:reply, Map.fetch(names, name), names}
+  ## Server Callbacks
+
+  @doc """
+  Start the registry.
+  """
+  def init(:ok) do
+    names = %{}
+    refs = %{}
+    {:ok, {names, refs}}
   end
 
-  def handle_cast({:create, name}, names) do
+  def handle_call({:lookup, name}, _from, {names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
+  end
+
+  def handle_cast({:create, name}, {names, refs}) do
     if Map.has_key?(names, name) do
-      {:noreply, names}
+      {:noreply, {names, refs}}
     else
-      {:ok, bucket} = KV.Bucket.start_link([])
-      {:noreply, Map.put(names, name, bucket)}
+      {:ok, pid} = KV.Bucket.start_link([])
+      ref = Process.monitor(pid)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, pid)
+      {:noreply, {names, refs}}
     end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
   end
 end
